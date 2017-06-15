05cceb3d981cb0e6695b552291cb7ec02e19672a
Cristina Iannone <iannonecristina@gmail.com>
2017-06-15 17:45:13 +0100

    Add tests for any item, Aged Brie and quality value

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,19 +3,78 @@ require_relative '../gilded_rose.rb'
 
 describe GildedRose do
 
-  context "any item except 'Aged Brie', 'Sulfura, hand of Ragnaros' and 'Backstage passes'" do
+  context "Any item except 'Aged Brie', 'Sulfura, hand of Ragnaros' and 'Backstage passes'" do
     describe "#update_quality" do
+
       it "does not change the name" do
         items = [Item.new("foo", 10, 10)]
         GildedRose.new(items).update_quality()
         expect(items[0].name).to eq "foo"
       end
 
-      it "changes the sell in date by -1" do
+      it "changes the sell in value by -1" do
         items = [Item.new("foo", 10, 10)]
         inventory = GildedRose.new(items)
         expect { inventory.update_quality }.to change{ items[0].sell_in}.by -1
       end
+
+      it "changes the quality value by -1" do
+        items = [Item.new("foo", 10, 10)]
+        inventory = GildedRose.new(items)
+        expect { inventory.update_quality }.to change{ items[0].quality}.by -1
+      end
+
+      it "changes the quality value by -2 when item passes the sell by date" do
+        items = [Item.new("foo", 0, 3)]
+        inventory = GildedRose.new(items)
+        expect { inventory.update_quality }.to change{ items[0].quality}.by -2
+      end
+
+      it "the quality of an item is never negative" do
+        items = [Item.new("foo", 2, 1)]
+        inventory = GildedRose.new(items)
+        inventory.update_quality
+        expect { inventory.update_quality }.to_not change{ items[0].quality}
+      end
     end
   end
+
+  context "Aged Brie" do
+    describe "#update_quality" do
+      it "increases in quality the older it gets" do
+        items = [Item.new("Aged Brie", 10, 15)]
+        inventory = GildedRose.new(items)
+        expect { inventory.update_quality }.to change{ items[0].quality}.by 1
+      end
+
+      it "doesn't increase in quality beyond 50" do
+        items = [Item.new("Aged Brie", 10, 50)]
+        inventory = GildedRose.new(items)
+        expect { inventory.update_quality }.to_not change{ items[0].quality}
+      end
+    end
+  end
+
+  context "The Quality of an item is never more than 50" do
+    describe "#update_quality" do
+      it "Aged Brie's quality cannot go beyond 50" do
+        items = [Item.new("Aged Brie", 10, 50)]
+        inventory = GildedRose.new(items)
+        expect { inventory.update_quality }.to_not change{ items[0].quality}
+      end
+
+      it "Backstage passes' quality cannot go beyond 50" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)]
+        inventory = GildedRose.new(items)
+        expect { inventory.update_quality }.to_not change{ items[0].quality}
+      end
+
+      it "Sulfuras quality cannot go beyond 50" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 50)]
+        inventory = GildedRose.new(items)
+        expect { inventory.update_quality }.to_not change{ items[0].quality}
+      end
+    end
+  end
+
 end
