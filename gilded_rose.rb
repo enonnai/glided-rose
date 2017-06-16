303165460373d437cd15d36fb0c0d1ae4455083e
Cristina Iannone <iannonecristina@gmail.com>
2017-06-16 11:08:04 +0100

    Refactor #update_quality based on context: Any item except 'Aged Brie', 'Sulfura, hand of Ragnaros' and 'Backstage passes'

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,55 +1,27 @@
 class GildedRose
 
+  attr_accessor :items
+
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+      if not_a_special_item?(item) && item.sell_in > 0
+        item.sell_in -= 1
+        item.quality = item.quality - 1 unless item.quality == 0
+      elsif not_a_special_item?(item) && item.sell_in <= 0
+        item.sell_in -= 1
+        item.quality -= 2 unless item.quality == 0
       end
     end
+  end
+
+  private
+
+  def not_a_special_item?(item)
+    item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
   end
 end
 
